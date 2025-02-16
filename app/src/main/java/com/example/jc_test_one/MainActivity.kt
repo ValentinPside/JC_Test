ecb9a3aaef14701367a75c1958054eb26232dfc6
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2025-02-16 22:07:33 +0300

    InstagramProfileCard2

--- app/src/main/java/com/example/jc_test_one/MainActivity.kt
+++ app/src/main/java/com/example/jc_test_one/MainActivity.kt
@@ -3,18 +3,13 @@ package com.example.jc_test_one
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,7 +21,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramProfileCard()
+            CardTest()
         }
+    }
+}
+
+@Preview
+@Composable
+fun CardTest(){
+    Card(
+        shape = RoundedCornerShape(
+            topEnd = 4.dp,
+            topStart = 4.dp
+        ),
+        border = BorderStroke(1.dp, Color.Black)
+    ) {
+        InstagramProfileCard()
     }
 }
