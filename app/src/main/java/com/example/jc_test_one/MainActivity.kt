94cb4e3679cc8508ae85b8b1818adc9bc0010451
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2025-02-17 15:32:02 +0300

    Image and Text

--- app/src/main/java/com/example/jc_test_one/MainActivity.kt
+++ app/src/main/java/com/example/jc_test_one/MainActivity.kt
@@ -3,39 +3,28 @@ package com.example.jc_test_one
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.jc_test_one.ui.theme.InstagramProfileCard
+import com.example.jc_test_one.ui.theme.CardTest
+import com.example.jc_test_one.ui.theme.JC_Test_OneTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardTest()
+            JC_Test_OneTheme() {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    CardTest()
+                }
+            }
         }
-    }
-}
-
-@Preview
-@Composable
-fun CardTest(){
-    Card(
-        shape = RoundedCornerShape(
-            topEnd = 4.dp,
-            topStart = 4.dp
-        ),
-        border = BorderStroke(1.dp, Color.Black)
-    ) {
-        InstagramProfileCard()
     }
 }
--- app/src/main/java/com/example/jc_test_one/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/example/jc_test_one/ui/theme/InstagramProfileCard.kt
@@ -1,60 +1,103 @@
 package com.example.jc_test_one.ui.theme
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jc_test_one.R
 
-@Preview
 @Composable
-fun InstagramProfileCard(){
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically)
-    {
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(color = Color.Yellow)
-        ){
-
-        }
-        TowBoxes()
-        TowBoxes()
-        TowBoxes()
+fun CardTest() {
+    Card(
+        modifier = Modifier
+            .padding(8.dp),
+        shape = RoundedCornerShape(
+            topEnd = 4.dp,
+            topStart = 4.dp
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground)
+    ) {
+        InstagramProfileCard()
     }
 }
 
 @Composable
-private fun TowBoxes(){
-    Column (modifier = Modifier
-        .height(80.dp),
+fun InstagramProfileCard() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    )
+    {
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(50.dp),
+            painter = painterResource(id = R.drawable.instagram_icon),
+            contentDescription = null
+        )
+        UserStatistics(title = "Posts", value = "6,950")
+        UserStatistics(title = "Followers", value = "436M")
+        UserStatistics(title = "Following", value = "76")
+    }
+}
+
+@Composable
+private fun UserStatistics(
+    title: String, value: String
+) {
+    Column(
+        modifier = Modifier
+            .height(80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
-    ){
-        Box(modifier = Modifier
-            .size(25.dp)
-            .background(color = Color.Black)
-        ){
+    ) {
+        Text(
+            text = value,
+            fontSize = 24.sp,
+            fontFamily = FontFamily.Cursive
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
-        }
-        Box(modifier = Modifier
-            .size(25.dp)
-            .background(color = Color.Blue)
-        ){
+@Preview
+@Composable
+private fun PreviewCardDark() {
+    JC_Test_OneTheme(darkTheme = true) {
+        CardTest()
+    }
+}
 
-        }
+@Preview
+@Composable
+private fun PreviewCardLight() {
+    JC_Test_OneTheme(darkTheme = false) {
+        CardTest()
     }
 }
