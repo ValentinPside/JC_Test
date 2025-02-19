aa578f669e6e233a8ba3f97b3073712a84633eef
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2025-02-19 23:12:20 +0300

    Navigation

---
+++ app/src/main/java/com/example/jc_test_one/ui/theme/VkMainScreen.kt
@@ -0,0 +1,14 @@
+package com.example.jc_test_one.ui.theme
+
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+    Scaffold (
+        bottomBar = {
+            
+        }
+    ){  }
+}
